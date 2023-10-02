5c5b2b7ea42b80d3be38ceb491a0a1fb1045b02e
WackyGlow <70566908+WackyGlow@users.noreply.github.com>
2023-10-02 10:25:24 +0200

    Initial commit

---
+++ PMan/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PMan
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
